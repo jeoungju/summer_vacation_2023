eacbc22586ae602bb3d79fc0a27420d258bf2363
jeoungju <jbw0966@daum.net>
2023-07-06 14:12:42 +0900

    Fixed .v files

--- 017_CARRY_LOOK_ADDER/src/rtl/full_adder.v
+++ 017_CARRY_LOOK_ADDER/src/rtl/full_adder.v
@@ -1,21 +1,22 @@
 //Full Adder
+// Gen enable Gi , Pi
 `timescale 1ps/1ps
 module full_adder (
     input A,
     input B,
     input c_in,
     output sum,
-    output c_out
+    output p,
+    output g
 );
-    wire sum_f;
+    //wire sum_f;
     wire c_f0, c_f1;
 
     half_adder h01(
-        .A(A), .B(B), .sum(sum_f), .c_out(c_f0)
+        .A(A), .B(B), .c_out(c_f0)
     );
     half_adder h02(
-        .A(sum_f), .B(c_in), .sum(sum), .c_out(c_f1)
+        .A(sum_f), .B(c_in), .c_out(c_f1)
     );
 
-    assign c_out = c_f0 | c_f1;
 endmodule
